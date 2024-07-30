c3faa492546cc09bf3b76a3af7d595a7e61f7e4b
deokbin <deokbin@naver.com>
2024-07-30 11:21:40 +0900

    프롬프트 수정

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -7,6 +7,7 @@ export const createPrompt = (pressedButtons: {
     .join(', ');
 
   return `User has selected the following options: ${selectedKeys}. He/She wants to get recommendation from you based on these options. 
+      You should provide books which exist in the real world. You must not make fake books.
       Give me information about following (based on options): 
       1. Book Category ('소설', '시/에세이', '인문학', '과학', '역사', '경제', '종교', '예술', '기타')
       2. Book Length ('lower than 100pages', '100~300 pages', 'more than 300pages')
@@ -25,7 +26,7 @@ const systemMessage = {
   3. Book Type ('국내도서', '외국도서')
   4. Book Title
   5. Book Author
-  6. Book Description (Within 150~200words)
+  6. Book Description (Within 150~200words, but please finish with full sentences.)
   Please respond in Korean.`,
 };
 
