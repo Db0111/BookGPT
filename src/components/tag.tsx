18670a91a2982ee36dea89476ec80e3773394769
deokbin <deokbin@naver.com>
2024-08-02 16:40:58 +0900

    Tag 컴포넌트 추가

---
+++ src/components/tag.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+export const Tag = ({title}: {title: string}) => {
+  return (
+    <TouchableOpacity style={styles.tag}>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+export default Tag;
+
+const styles = StyleSheet.create({
+  tag: {
+    borderColor: 'black',
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 18,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingRight: 10,
+    paddingLeft: 10,
+  },
+});
